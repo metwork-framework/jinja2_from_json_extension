f24bfea5d418960f2f188f635ef583148890fc5c
thebaptiste <jean-baptiste.veslin@meteo.fr>
2025-05-23 12:34:42 +0000

    build: fix flake8 warnings

--- jinja2_from_json_extension/__init__.py
+++ jinja2_from_json_extension/__init__.py
@@ -1,5 +1,4 @@
 import json
-import jinja2
 from jinja2.ext import Extension
 
 
@@ -7,6 +6,7 @@ try:
     from jinja2 import pass_eval_context as eval_context
 except ImportError:
     from jinja2 import evalcontextfilter as eval_context
+
 
 @eval_context
 def from_json(eval_ctx, value):
